b3509da952f24260d51fad4cb10f41a82afea5c6
Ben Clothier <ben@exabytegames.com>
2022-02-05 12:14:23 +0000

    Add descriptions for project1 and project2

--- src/pages/Project1.js
+++ src/pages/Project1.js
@@ -4,6 +4,7 @@ function Project1() {
     return (
         <div className="mainpage">
             <h1>FINAL OUTPOST</h1>
+            <p>You have been chosen as the leader of what you think is the last remaining outpost. You must manage your citizens and resources in order to build an impenetrable fortress. How long can you survive against the zombie onslaught?</p>
         </div>
     );
   }
--- src/pages/Project3.js
+++ src/pages/Project3.js
@@ -4,6 +4,7 @@ function Project3() {
     return (
         <div className="mainpage">
             <h1>ALIEN</h1>
+            <p>A small arcade-style game based around the Alien franchise. You, Ellen Ripley, are tasked with searching the ship for the crew's cat, Jonesy, and escaping safely via the last remaining escape pod. However, there's something else lurking aboard; do you move quickly and risk getting hunted down, or do you move silently and risk it being too late to escape?</p>
         </div>
     );
   }

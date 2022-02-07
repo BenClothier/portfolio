5ee586078d03d76d761b0d78f461f42b76d70f5f
Ben Clothier <ben@exabytegames.com>
2022-02-07 14:02:27 +0000

    Add captions to galleries

--- src/pages/Project1.js
+++ src/pages/Project1.js
@@ -14,13 +14,17 @@ function Project1() {
             <p>You have been chosen as the leader of what you think is the last remaining outpost. You must manage your citizens and resources in order to build an impenetrable fortress. How long can you survive against the zombie onslaught?</p>
             <div className="gallery">
                 <img id="picture1" src={picture1} alt="picture1"/>
+                <div className="caption"><p>Final Outpost main menu</p></div>
                 <iframe src="https://www.youtube.com/embed/XCUyMTKVGGg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
                 <iframe src="https://www.youtube.com/embed/7Pa68msNSBk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
                 <iframe src="https://www.youtube.com/embed/veLgNk3mhZc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
                 <iframe src="https://www.youtube.com/embed/W3cpfulhr6I" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
                 <img id="picture2" src={picture2} alt="picture2"/>
+                <div className="caption"><p>Viewing the skill tree</p></div>
                 <img id="picture3" src={picture3} alt="picture3"/>
+                <div className="caption"><p>Playing the game in german</p></div>
                 <img id="picture4" src={picture4} alt="picture4"/>
+                <div className="caption"><p>Viewing achievements in Game Center</p></div>
             </div>
         </div>
     );
--- src/pages/Project2.js
+++ src/pages/Project2.js
@@ -12,10 +12,13 @@ function Project2() {
             <p>A 3D, first-person demo project focusing on multiplayer, skeletal animation, and wacky physics. Served as both an adventure into various aspects of game programming as well as proof of ability in the Unity engine. </p>
             <div className="gallery">
                 <iframe src="https://www.youtube.com/embed/Tdv7wfd8MCM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
+                <div className="caption"><p>Demonstration of multiplayer synchronisation across two running instances of the prototype</p></div>
                 <img id="picture1" src={picture1} alt="picture1"/>
+                <div className="caption"><p>Navigation mesh for NPCs</p></div>
                 <iframe src="https://www.youtube.com/embed/hLU0HURoF-w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
+                <div className="caption"><p>Playing the whole catalogue of character animations in sequence</p></div>
                 <img id="picture2" src={picture2} alt="picture2"/>
-
+                <div className="caption"><p>First-person hands model (*gun model imported from the Unity asset store)</p></div>
             </div>
         </div>
     );

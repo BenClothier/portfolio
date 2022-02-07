ca1d436ebad6e5616fd1f3074db920ac4881b39c
Ben Clothier <ben@exabytegames.com>
2022-02-07 13:42:38 +0000

    Add new final outpost content

--- src/pages/Project1.js
+++ src/pages/Project1.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import picture1 from "../images/final-outpost/1.png"
 import picture2 from "../images/final-outpost/2.png"
+import picture3 from "../images/final-outpost/3.png"
+import picture4 from "../images/final-outpost/4.png"
 
 function Project1() {
     return (
@@ -11,9 +13,14 @@ function Project1() {
             <h2>Lead Programmer | Unity/C# | iOS/Android</h2>
             <p>You have been chosen as the leader of what you think is the last remaining outpost. You must manage your citizens and resources in order to build an impenetrable fortress. How long can you survive against the zombie onslaught?</p>
             <div className="gallery">
-                <iframe src="https://www.youtube.com/embed/XCUyMTKVGGg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
                 <img id="picture1" src={picture1} alt="picture1"/>
+                <iframe src="https://www.youtube.com/embed/XCUyMTKVGGg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
+                <iframe src="https://www.youtube.com/embed/7Pa68msNSBk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
+                <iframe src="https://www.youtube.com/embed/veLgNk3mhZc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
+                <iframe src="https://www.youtube.com/embed/W3cpfulhr6I" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
                 <img id="picture2" src={picture2} alt="picture2"/>
+                <img id="picture3" src={picture3} alt="picture3"/>
+                <img id="picture4" src={picture4} alt="picture4"/>
             </div>
         </div>
     );

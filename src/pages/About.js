7744786c71b415c798f5fd577dd0c847af67a1c3
Ben Clothier <ben@exabytegames.com>
2022-02-08 22:25:04 +0000

    Add info to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,11 +4,31 @@ function About() {
     return (
         <div className="mainpage">
             <h1>ABOUT</h1>
-            <p>2nd year student at University of Southampton. 
-                I have always been into games and so naturally I have found a burning passion for game development. 
-                I’m determined to become an expert game programmer and therefore want to gain as much experience as I 
-                can.
-            </p>
+            <div className="card">
+                
+                <p>
+                    Hey! I'm Ben, a game programmer and designer living in the UK. I have recently released a game called Final Outpost on the App Store and Google Play Store with Exabyte Games. I am currently studying computer science at the University of Southampton.
+                </p>
+                <p>  
+                    Here you can see the cool projects I’ve worked on that I think are significant to me. Although it’s currently a small list, I’m always looking for fun game-dev projects when I get the time and I also intend to take on further professional projects soon; watch this space!
+                </p>
+                <p>      
+                    Feel free to contact me if you want to know more :)
+
+                </p>
+            </div>
+            <h1>SKILLS</h1>
+            <div className="card">
+                
+                <div class="row">
+                    <div class="column">
+                        <h3>Languages</h3>
+                    </div>
+                    <div class="column">
+                        <h3>Tools</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
   }

e4241ca5c312680235298e18569b5c64868c9f4e
Ben Clothier <ben@exabytegames.com>
2022-02-26 16:27:51 +0000

    Add skills and future projects

--- src/pages/About.js
+++ src/pages/About.js
@@ -37,6 +37,8 @@ function About() {
                         <div className="skillbox"><p>Haskell</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
                         <div className="divider" />
                         <div className="skillbox"><p>C++</p> <img id="oneStar" src={oneStar} alt="oneStar"/></div>
+                        <div className="divider" />
+                        <div className="skillbox"><p>HTML / CSS</p> <img id="oneStar" src={oneStar} alt="oneStar"/></div>
                     </div>
                     <div class="column">
                         <h2>Tools</h2>
@@ -50,6 +52,8 @@ function About() {
                         <div className="skillbox"><p>Affinity Photo</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
                         <div className="divider" />
                         <div className="skillbox"><p>JetBrains IDEs</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
+                        <div className="divider" />
+                        <div className="skillbox"><p>OpenGL</p> <img id="oneStar" src={oneStar} alt="oneStar"/></div>
                         <div className="divider" />
                         <div className="skillbox"><p>Blender</p> <img id="oneStar" src={oneStar} alt="oneStar"/></div>
                     </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,6 +12,8 @@ import projectcover1 from "../images/Title.png"
 import projectcover2 from "../images/NavMesh.png"
 import projectcover3 from "../images/AlienMenu.png"
 import projectcover4 from "../images/coming-soon.png"
+import projectcover5 from "../images/open-gl.png"
+import projectcover6 from "../images/KIRK.png"
 
 function Projects() {
     return (
@@ -21,13 +23,13 @@ function Projects() {
                 <Link to="/project1">
                     <div className="imagebutton">
                         <img id="projectcover1" src={projectcover1} alt="Final Outpost"/>
-                        <div className="hiddentext">FINAL OUTPOST</div>
+                        <div className="hiddentext">Final Outpost</div>
                     </div>
                 </Link>
                 <Link to="/project2">
                     <div className="imagebutton">
                         <img id="projectcover2" src={projectcover2} alt="MULTIPLAYER SHOOTER DEMO"/>
-                        <div className="hiddentext">MULTIPLAYER SHOOTER DEMO</div>
+                        <div className="hiddentext">Multiplayer Shooter Demo</div>
                     </div>
                 </Link>
                 <Link to="/project3">
@@ -37,11 +39,33 @@ function Projects() {
                     </div>
                 </Link>
                 
-                <div className="divider" />
-                <div className="imagebutton">
-                    <img id="projectcover1" src={projectcover4} alt="coming-soon"/>
-                    <div className="hiddentext">= COMING SOON =</div>
+                <div>
+                    <div className="divider"/>
+                    <h2>IN PROGRESS</h2>
                 </div>
+                <Link to="">
+                <div className="imagebutton">
+                    <img id="projectcover4" src={projectcover4} alt="coming-soon"/>
+                    <div className="hiddentext">New Unity Game</div>
+                </div>
+                </Link>
+                <Link to="">
+                <div className="imagebutton">
+                    <img id="projectcover5" src={projectcover5} alt="OpenGL"/>
+                    <div className="hiddentext">OpenGL Test Game</div>
+                </div>
+                </Link>
+
+                <div>
+                    <div className="divider"/>
+                    <h2>FUTURE PROJECTS</h2>
+                </div>
+                <Link to="">
+                <div className="imagebutton">
+                    <img id="projectcover6" src={projectcover6} alt="KIRK"/>
+                    <div className="hiddentext">"KIRK"<br/>The Unity Build Server</div>
+                </div>
+                </Link>
             </div>
         </div>
     );

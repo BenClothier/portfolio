7d19ab5313fefa68e0fe189f4157842d1208f03e
Ben Clothier <ben@exabytegames.com>
2022-03-02 08:27:29 +0000

    Fix imagebutton bounds

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -20,52 +20,51 @@ function Projects() {
         <div className="mainpage">
             <h1>PROJECTS</h1>
             <div className="buttonpanel">
-                <Link to="/project1">
-                    <div className="imagebutton">
+                <div>
+                    <Link to="/project1" className="imagebutton">
                         <img id="projectcover1" src={projectcover1} alt="Final Outpost"/>
                         <div className="hiddentext">Final Outpost</div>
-                    </div>
-                </Link>
-                <Link to="/project2">
-                    <div className="imagebutton">
-                        <img id="projectcover2" src={projectcover2} alt="MULTIPLAYER SHOOTER DEMO"/>
-                        <div className="hiddentext">Multiplayer Shooter Demo</div>
-                    </div>
-                </Link>
-                <Link to="/project3">
-                    <div className="imagebutton">
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/project2" className="imagebutton">
+                    <img id="projectcover2" src={projectcover2} alt="MULTIPLAYER SHOOTER DEMO"/>
+                    <div className="hiddentext">Multiplayer Shooter Demo</div>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/project3" className="imagebutton">
                         <img id="projectcover3" src={projectcover3} alt="ALIEN"/>
                         <div className="hiddentext">ALIEN</div>
-                    </div>
-                </Link>
-                
+                    </Link>
+                </div>
                 <div>
                     <div className="divider"/>
                     <h2>IN PROGRESS</h2>
                 </div>
-                <Link to="">
+                <div>
                 <div className="imagebutton">
                     <img id="projectcover4" src={projectcover4} alt="coming-soon"/>
                     <div className="hiddentext">New Unity Game</div>
                 </div>
-                </Link>
-                <Link to="">
+                </div>
+                <div>
                 <div className="imagebutton">
                     <img id="projectcover5" src={projectcover5} alt="OpenGL"/>
                     <div className="hiddentext">OpenGL Test Game</div>
                 </div>
-                </Link>
+                </div>
 
                 <div>
                     <div className="divider"/>
                     <h2>FUTURE PROJECTS</h2>
                 </div>
-                <Link to="">
+                <div>
                 <div className="imagebutton">
                     <img id="projectcover6" src={projectcover6} alt="KIRK"/>
                     <div className="hiddentext">"KIRK"<br/>The Unity Build Server</div>
                 </div>
-                </Link>
+                </div>
             </div>
         </div>
     );

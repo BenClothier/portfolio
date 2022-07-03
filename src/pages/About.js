f060979137a2820003cdfb27c4bd2c3d6584008c
Ben Clothier <ben.clothier8@icloud.com>
2022-07-03 10:33:56 +0100

    Change profile picture and update text

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,7 +14,7 @@ function About() {
                     Hey! I'm Ben, a game programmer and designer living in the UK. I have recently released a game called Final Outpost on the App Store and Google Play Store with Exabyte Games. I am currently studying computer science at the University of Southampton.
                 </p>
                 <p>  
-                    Here you can see the cool projects I’ve worked on that I think are significant to me. Although it’s currently a small list, I’m always looking for fun game-dev projects when I get the time and I also intend to take on further professional projects soon; watch this space!
+                    Here you can see the cool projects I’ve worked on that I think are significant to me. Although it’s currently a small list, I’m always looking for fun game-dev projects when I get the time and I also intend to take on further professional projects soon - watch this space!
                 </p>
                 <p>      
                     Feel free to contact me if you want to know more :)
@@ -28,13 +28,13 @@ function About() {
                         <div className="divider" />
                         <div className="skillbox"><p>C#</p> <img id="threeStars" src={threeStars} alt="threeStars"/></div>
                         <div className="divider" />
-                        <div className="skillbox"><p>Python</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
+                        <div className="skillbox"><p>C++</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
                         <div className="divider" />
                         <div className="skillbox"><p>C</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
                         <div className="divider" />
-                        <div className="skillbox"><p>Java</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
+                        <div className="skillbox"><p>Python</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
                         <div className="divider" />
-                        <div className="skillbox"><p>C++</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
+                        <div className="skillbox"><p>Java</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
                         <div className="divider" />
                         <div className="skillbox"><p>Haskell</p> <img id="oneStar" src={oneStar} alt="oneStar"/></div>
                         <div className="divider" />
@@ -45,13 +45,13 @@ function About() {
                         <div className="divider" />
                         <div className="skillbox"><p>Unity</p> <img id="threeStars" src={threeStars} alt="threeStars"/></div>
                         <div className="divider" />
-                        <div className="skillbox"><p>Visual Studio</p> <img id="threeStars" src={threeStars} alt="threeStars"/></div>
-                        <div className="divider" />
                         <div className="skillbox"><p>GitHub / GitKraken</p> <img id="threeStars" src={threeStars} alt="threeStars"/></div>
                         <div className="divider" />
-                        <div className="skillbox"><p>Affinity Photo</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
+                        <div className="skillbox"><p>Visual Studio</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
                         <div className="divider" />
                         <div className="skillbox"><p>JetBrains IDEs</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
+                        <div className="divider" />
+                        <div className="skillbox"><p>Affinity Photo</p> <img id="twoStars" src={twoStars} alt="twoStars"/></div>
                         <div className="divider" />
                         <div className="skillbox"><p>OpenGL</p> <img id="oneStar" src={oneStar} alt="oneStar"/></div>
                         <div className="divider" />
